ffcca8c516d7467da793d165f43b6c9508c2fc81
Daud Zoss <daud.zoss@philips.com>
2024-03-13 18:15:56 -0700

    unable to route CLBSWIN_SFR_HOLD_SYNCHRONIZED

--- clb/quadenc/qe.v
+++ clb/quadenc/qe.v
@@ -1,7 +1,17 @@
-module qe(i, q, clr, clk, /*y15,y14,y13,y12,y11,y10,y9,y8,y7,y6,y5,y4,*/y3,y2,y1,y0);
-   input i, q, clr, clk;
+module qe(clk,
+	  /*a15,a14,a13,a12,a11,a10,a9,a8,a7,a6,a5,a4,*/a3,a2,a1,a0,update,
+	  i, i_r, i_f,
+	  q, q_r, q_f,
+	  /*y15,y14,y13,y12,y11,y10,y9,y8,y7,y6,y5,y4,*/y3,y2,y1,y0,c,b);
+
+   input clk;
+   input /*a15,a14,a13,a12,a11,a10,a9,a8,a7,a6,a5,a4,*/a3,a2,a1,a0,update;   
+   input i, i_r, i_f;
+   input q, q_r, q_f;
 
    output /*y15,y14,y13,y12,y11,y10,y9,y8,y7,y6,y5,y4,*/y3,y2,y1,y0;
+   output c,b;
+   
    reg [3/*15*/:0] value;
 /*   assign y15 = value[15];
    assign y14 = value[14];
@@ -20,35 +30,44 @@ module qe(i, q, clr, clk, /*y15,y14,y13,y12,y11,y10,y9,y8,y7,y6,y5,y4,*/y3,y2,y1
    assign y1 = value[1];
    assign y0 = value[0];
 
-   wire i_or_q, r_or_f;
-
-   always @(posedge i)
-     begin
-       i_or_q = 0;
-       r_or_f = 1;
-     end
-
-   always @(negedge i)
-     begin
-       i_or_q = 0;
-       r_or_f = 0;
-     end
-
-   always @(posedge q)
-     begin
-       i_or_q = 1;
-       r_or_f = 1;
-     end
-
-   always @(negedge q)
-     begin
-       i_or_q = 1;
-       r_or_f = 0;
-     end
-
    always @(posedge clk)
-     if (clr)
-       value = 4/*16*/'b0;
-     else
-       value = value + (i_or_q ^ r_or_f ^ (i_or_q?i:q)) ? +1 : -1;
+     begin
+       if (update)
+	 begin
+	    value[3] = a3;
+	    value[2] = a2;
+	    value[1] = a1;
+	    value[0] = a0;
+	    c = 0;
+	    b = 0;
+	 end
+
+       else
+	 begin
+	    if ((i_r & ~q) | (i_f & q) | (q_r & i) | (q_f & ~i))
+	      begin
+		 value += 1;
+		 c = (~value) ? 1 : 0;
+		 b = 0;
+	      end
+	    else
+	      begin
+		 value -= 1;
+		 c = 0;
+		 b = value ? 0 : 1;
+	      end
+/*
+            if (i_r)
+ 	      value += q ? -1 : +1;
+	    else if (i_f)
+	      value += q ? +1 : -1;
+	    else if (q_r)
+	      value += i ? +1 : -1;
+	    else if (q_f)
+	      value += i ? -1 : +1;
+*/	    
+	 end // else: !if(update)
+     end // always @ (posedge clk)
+
+
 endmodule
