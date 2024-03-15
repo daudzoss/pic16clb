d4fefcf7c1337261b6dc3689de25a1b71b172e0f
Daud Zoss <daud.zoss@philips.com>
2024-03-15 12:21:49 -0700

    still unable with constant reset value of 0 instead of a register-specified reset value

--- clb/quadenc/qe.v
+++ clb/quadenc/qe.v
@@ -34,10 +34,10 @@ module qe(clk,
      begin
        if (update)
 	 begin
-	    value[3] = a3;
+/*	    value[3] = a3;
 	    value[2] = a2;
 	    value[1] = a1;
-	    value[0] = a0;
+	    value[0] = a0;*/	    value = 4'b0;
 	    c = 0;
 	    b = 0;
 	 end
